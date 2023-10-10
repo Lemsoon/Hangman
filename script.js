7631567e63dae406595b20f4f66175ee1e450a58
Sebastian <selfishworm117@gmail.com>
2023-10-10 09:44:20 +0200

    prints userinput to console

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("guessButton").addEventListener("click", guess);
+
+function guess() {
+  let userInput = document.getElementById("userInput");
+  console.log(userInput.value);
+}

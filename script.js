a60f3d90c247811dc3cc9ca03b679765cceea195
Sebastian <selfishworm117@gmail.com>
2023-10-10 11:46:39 +0200

    added a seperate js file for alphabet

--- script.js
+++ script.js
@@ -66,20 +66,3 @@ function guess() {
     console.log(chosenWord, "does not have ", userInput.value, " in it.");
   }
 }
-
-//function for the alphabets (keyboard)
-let alphabet = [];
-
-//ASCII values of A= 65 and Z=90
-for (let i = 65; i <= 90; i++) {
-  alphabet.push(String.fromCharCode(i));
-}
-
-let alphabetContainer = document.getElementById("alphabet-container");
-
-// adding each letter to keyboard-container
-for (let j = 0; j < alphabet.length; j++) {
-  let letterElement = document.createElement("span");
-  letterElement.textContent = alphabet[j];
-  alphabetContainer.appendChild(letterElement);
-}

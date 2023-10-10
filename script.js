369781245c302cec04497b239faa4db2e5d2eb30
Esmat Najafi <145345735+Esmatnajafi@users.noreply.github.com>
2023-10-10 10:56:45 +0200

    commit

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ function guess() {
   let userInput = document.getElementById("userInput");
   console.log(userInput.value);
 }
+
+//function for the alphabets (keyboard)
+let alphabet = []; 
+
+
+//ASCII values of A= 65 and Z=90
+for (let i = 65; i <= 90; i++) {
+  alphabet.push(String.fromCharCode(i));  
+}
+
+let keyboardContainer = document.querySelector("keyboard-container");
+
+
+// adding each letter to keyboard-container
+for (let j = 0; j < alphabet.length; j++) {
+  letterElemet.textContetn = alphabet[j];
+  keyboardContainer.appendChild(letterElemet); 
+}
+
+console.log(keyboardContainer); 

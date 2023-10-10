416f9d1dca7394b48d2da9c6bbd7fad131c531a3
Esmat Najafi <145345735+Esmatnajafi@users.noreply.github.com>
2023-10-10 11:37:35 +0200

    commit

--- script.js
+++ script.js
@@ -75,13 +75,12 @@ for (let i = 65; i <= 90; i++) {
   alphabet.push(String.fromCharCode(i));
 }
 
-let keyboardContainer = document.getElementById("keyboard-container");
+let alphabetContainer = document.getElementById("alphabet-container");
 
 // adding each letter to keyboard-container
 for (let j = 0; j < alphabet.length; j++) {
   let letterElement = document.createElement("span"); 
   letterElement.textContent = alphabet[j];
-  keyboardContainer.appendChild(letterElemet);
+  alphabetContainer.appendChild(letterElement);
 }
 
-console.log(keyboardContainer);

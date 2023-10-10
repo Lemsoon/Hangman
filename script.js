c33b86dcf5e63f0ec346dff6e51f8d80d54bcbfc
Sebastian <selfishworm117@gmail.com>
2023-10-10 10:58:51 +0200

    added id on start game and reset game

--- script.js
+++ script.js
@@ -68,21 +68,19 @@ function guess() {
 }
 
 //function for the alphabets (keyboard)
-let alphabet = []; 
-
+let alphabet = [];
 
 //ASCII values of A= 65 and Z=90
 for (let i = 65; i <= 90; i++) {
-  alphabet.push(String.fromCharCode(i));  
+  alphabet.push(String.fromCharCode(i));
 }
 
 let keyboardContainer = document.querySelector("keyboard-container");
 
-
 // adding each letter to keyboard-container
 for (let j = 0; j < alphabet.length; j++) {
   letterElemet.textContetn = alphabet[j];
-  keyboardContainer.appendChild(letterElemet); 
+  keyboardContainer.appendChild(letterElemet);
 }
 
-console.log(keyboardContainer); 
+console.log(keyboardContainer);

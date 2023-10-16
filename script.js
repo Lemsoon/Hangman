332bacccc52a8d8b6e084fceb0fe77190b1d1136
Sebastian <selfishworm117@gmail.com>
2023-10-16 13:40:11 +0200

    what

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-
-
+//#region
 let hangedManSVG = document.querySelector("svg");
 let bodyparts = hangedManSVG.querySelectorAll("path");
 let looped = false;
+let letterBox;
 let correctLetters = 0;
 let win = false;
 let wordContainer = document.createElement("div");
 let main = document.querySelector("main");
 let letterButtons = [];
-let wrongGuesses = 0;
-let secretWord;
 const wordList = [
   "answer",
   "base",
@@ -156,53 +154,27 @@ const wordList = [
 ];
 
 main.appendChild(wordContainer);
-wordContainer.style = "display: flex; justify-content: center";
+wordContainer.style = "Display: flex; justify-content: center";
 document.getElementById("startButton").addEventListener("click", startGame);
 
 //#endregion
 
 function resetGame() {
   looped = false;
-  correctLetters = 0;
+  correctGuesses = 0;
   wrongGuesses = 0;
-  userInput.value = '';
   letterButtons = document.querySelectorAll("#alphabet-container>button");
   letterButtons.forEach((button) => {
-  //reset style 
-  button.style.backgroundColor = '';
-  button.style.margin = '';
-  button.style.border = '';
-  button.style.fontWeight = '';
-  button.style.height = '';
-  button.style.width = '';
-  button.removeEventListener("click", checkButton)
-  button.addEventListener("click", checkButton);
+    button.addEventListener("click", checkButton);
   });
 
   bodyparts.forEach((part) => {
     part.style.opacity = 0;
   });
-
-  wordContainer.innerHTML = '';
-
-  secretWord = wordList[Math.floor(Math.random() * wordList.length)];
-  secretWord = secretWord.toUpperCase();
-  secretWord = secretWord.split("")
-
-  secretWord.forEach((letter) => {
-    letterBox = document.createElement("p");
-    wordContainer.appendChild(letterBox);
-    letterBox.textContent = letter;
-    letterBox.style =
-      "width: .5rem; margin-left: .5rem; color: rgba(0, 0, 0, 0); text-decoration: underline 1px black ";
-  });
-  document.body.style.backgroundColor = "white";
-
 }
 
 function startGame() {
   resetGame();
-  /*
   secretWord = wordList[Math.floor(Math.random() * wordList.length)];
   secretWord = secretWord.toUpperCase();
   secretWord = secretWord.split("");
@@ -216,7 +188,6 @@ function startGame() {
     letterBox.style =
       "width: .5rem; margin-left: .5rem; color: rgba(0, 0, 0, 0); text-decoration: underline 1px black ";
   });
-  */
 }
 
 function checkButton(event) {
@@ -228,38 +199,40 @@ function checkButton(event) {
 
   if (!looped) {
     //i to compare to the lenght of a word, only when "i == wordLenght" will guess go up. goes up wordLenght amount each guess otherwise.
-    //let i = 0;
+    let i = 0;
     let correct = false;
-    console.log(secretWord)
-    //Checks if the secretWord array has the clicked letter in it
-    let index = 0;
-    secretWord.forEach((letter) => {
 
-      if (letter === chosenLetter) {
+    //Checks if the secretWord array has the clicked letter in it
+    secretWord.forEach((letter) => {
+      if (letter.includes(chosenLetter)) {
         correctLetters++;
         console.log("Correct letters: ", correctLetters);
         correct = true;
-
-        let letterBox = wordContainer.getElementsByTagName("p")[index];
-        letterBox.style.color = "rgba(0, 0, 0, 1)";
+        letterBoxArray = Array.from(wordContainer.getElementsByTagName("p"));
+        letterBoxArray.forEach((letterBox) => {
+          let letter = letterBox.textContent;
+          //Checks if chosen letter is in the word
+          if (chosenLetter == letter) {
+            letterBox.style.color = "rgba(0, 0, 0, 1)";
+          }
+        });
       }
       if (correctLetters == secretWord.length) {
         console.log("win");
         document.body.style.backgroundColor = "green";
         win = true;
-        document.getElementById("resultMessage").textContent = "Congratulations! You win!";
       }
-      index++;
+      i++;
     });
 
-    //if (i === secretWord.length) {
-    if (!correct) {
-      wrongGuesses++;
-      console.log("Wrong guesses: ", wrongGuesses);
-    }
+    if (i === secretWord.length) {
+      if (!correct) {
+        wrongGuesses++;
+        console.log("Wrong guesses: ", wrongGuesses);
+      }
 
-    looped = true;
-    //}
+      looped = true;
+    }
     hangHim(); //handles the unveiling of the hanging.
     return (looped = false), (correct = false);
   }
@@ -286,35 +259,3 @@ function hangHim() {
       break;
   }
 }
-let alphabet = [];
-
-for (let i = 65; i <= 90; i++) {
-  alphabet.push(String.fromCharCode(i));
-}
-
-let alphabetContainer = document.getElementById("alphabet-container");
-let userInput = document.createElement("input");
-// style input displya
-userInput.type = "text";
-userInput.style.width = "200px"; 
-userInput.style.padding = "10px";  
-userInput.style.border = "1px solid #ccc";  
-userInput.style.borderRadius = "5px";  
-userInput.style.fontFamily = "Arial, sans-serif";  
-userInput.style.fontSize = "16px";
-userInput.type = "text";
-alphabetContainer.appendChild(userInput);
-
-for (let j = 0; j < alphabet.length; j++) {
-  const letter = alphabet[j];
-  let letterElement = document.createElement("button");
-
-  letterElement.textContent = letter;
-  letterElement.addEventListener("click", function () {
-    userInput.value = letter;
-  });
-
-  alphabetContainer.appendChild(letterElement);
-}
-
-document.getElementById("resetButton").addEventListener("click", resetGame);
